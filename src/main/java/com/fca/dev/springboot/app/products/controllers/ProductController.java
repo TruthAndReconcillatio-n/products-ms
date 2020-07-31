241825e4712164bc4a817c44771e4fbdb90b1893
karunsane <karunsane@gmail.com>
2020-07-31 11:27:57 -0500

    Eureka Server Registry

--- src/main/java/com/fca/dev/springboot/app/products/controllers/ProductController.java
+++ src/main/java/com/fca/dev/springboot/app/products/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,9 @@ import com.fca.dev.springboot.app.products.models.service.IProductService;
 @RestController
 public class ProductController {
 	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@Autowired
 	private Environment env;
 	@Autowired
@@ -25,7 +29,7 @@ public class ProductController {
 	public List<Product> listar() {
 		
 		return productService.findAll().stream().map(product -> {
-			product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			product.setPort(port);
 			return product;
 			}).collect(Collectors.toList());
 	}
@@ -33,7 +37,7 @@ public class ProductController {
 	@GetMapping("/ver/{id}")
 	public Product detalle(@PathVariable Long id) {
 		Product product = productService.findById(id);
-		product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		product.setPort(port);
 		return product;
 	}
 }
